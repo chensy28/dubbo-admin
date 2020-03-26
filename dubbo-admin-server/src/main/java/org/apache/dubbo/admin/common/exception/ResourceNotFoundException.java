1da381a63ba8132a9df867a1cbc64bc685e211f9
陈胜勇 <chensy@tuya.com>
2020-03-26 23:42:58 +0800

    test

--- dubbo-admin-server/src/main/java/org/apache/dubbo/admin/common/exception/ResourceNotFoundException.java
+++ dubbo-admin-server/src/main/java/org/apache/dubbo/admin/common/exception/ResourceNotFoundException.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends SystemException {
 
+
     public ResourceNotFoundException() {
     }
 
